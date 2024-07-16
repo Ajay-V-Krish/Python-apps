6dc58890c02c7c5f5e08459f8a554f4c79191ea5
unknown <errormakesclever@gmail.com>
2024-07-16 17:43:05 +0530

    new files added

---
+++ New folder/main.py
@@ -0,0 +1,154 @@
+from tkinter import *
+from tkinter import ttk
+import sqlite3
+
+'''db = sqlite3.connect("Student.db")
+curses = db.cursor()
+curses.execute("CREATE TABLE IF NOT EXISTS Student(ID INTEGER, NAME VARCHAR(20), AGE INTEGER, DOB VARCHAR(20), GENDER VARCHAR(20), CITY VARCHAR(20))")
+db.commit()
+db.close()
+print('table created')'''
+
+class Student:
+    def __init__(self, main):
+        self.main = main
+        self.T_Frame = Frame(self.main, height=50, width=1200, background='aqua', bd=2, relief='groove')
+        self.T_Frame.pack()
+        self.Title = Label(self.T_Frame, text='Student Management System', font='TimesNewRoman 20 bold', width=1200, bg='aqua')
+        self.Title.pack()
+        
+        self.Frame_1 = Frame(self.main, height=580, width=420, bd=2, relief='groove', bg='aqua')
+        self.Frame_1.pack(side=LEFT)
+        self.Frame_1.pack_propagate(0) #It prevents the frame from decreasing the size and fixed to its same size.
+        
+        Label(self.Frame_1, text='Student Details', background='aqua', font='arial 12 bold').place(x=20, y=20)
+        
+        self.Id = Label(self.Frame_1, text='Id', bg='aqua', font='arial 11 bold')
+        self.Id.place(x=40,y=60)
+        self.Id_Entry = Entry(self.Frame_1, width=40)
+        self.Id_Entry.place(x=150, y=60)
+        
+        self.Name = Label(self.Frame_1, text='Name', bg='aqua', font='arial 11 bold')
+        self.Name.place(x=40,y=100)
+        self.Name_Entry = Entry(self.Frame_1, width=40)
+        self.Name_Entry.place(x=150, y=100)
+        
+        self.Age = Label(self.Frame_1, text='Age', bg='aqua', font='arial 11 bold')
+        self.Age.place(x=40,y=140)
+        self.Age_Entry = Entry(self.Frame_1, width=40)
+        self.Age_Entry.place(x=150, y=140)
+        
+        self.DOB = Label(self.Frame_1, text='DOB', bg='aqua', font='arial 11 bold')
+        self.DOB.place(x=40,y=180)
+        self.DOB_Entry = Entry(self.Frame_1, width=40)
+        self.DOB_Entry.place(x=150, y=180)
+        
+        self.Gender = Label(self.Frame_1, text='Gender', bg='aqua', font='arial 11 bold')
+        self.Gender.place(x=40,y=220)
+        self.Gender_Entry = Entry(self.Frame_1, width=40)
+        self.Gender_Entry.place(x=150, y=220)
+        
+        self.Aqua = Label(self.Frame_1, text='City', bg='aqua', font='arial 11 bold')
+        self.Aqua.place(x=40,y=260)
+        self.Aqua_Entry = Entry(self.Frame_1, width=40)
+        self.Aqua_Entry.place(x=150, y=260)
+        
+        #Buttons
+        self.Button_Frame = Frame(self.Frame_1, height=250, width=250, relief='groove', bd=2, bg='aqua')
+        self.Button_Frame.place(x=80,y=300)
+        
+        self.Add = Button(self.Button_Frame, text='Add', width=25, font='arial 11 bold', command=self.Add)
+        self.Add.pack()
+        self.Delete = Button(self.Button_Frame, text='Delete', width=25, font='arial 11 bold', command=self.Delete)
+        self.Delete.pack()
+        self.Update = Button(self.Button_Frame, text='Update', width=25, font='arial 11 bold', command=self.Update)
+        self.Update.pack()
+        self.Clear = Button(self.Button_Frame, text='Clear', width=25, font='arial 11 bold', command=self.Clear)
+        self.Clear.pack()
+        
+        #Buttons end
+        
+        self.Frame_2 = Frame(self.main, height=580, width=800, bd=2, relief='groove', bg='aqua')
+        self.Frame_2.pack(side=RIGHT)
+        
+        self.tree = ttk.Treeview(self.Frame_2, columns=('c1','c2','c3','c4','c5','c6'), show='headings', height=28)
+        
+        self.tree.column('#1', anchor=CENTER, width=40)
+        self.tree.heading('#1', text='ID')
+        
+        self.tree.column('#2', anchor=CENTER)
+        self.tree.heading('#2', text='Name')
+        
+        self.tree.column('#3', anchor=CENTER, width=115)
+        self.tree.heading('#3', text='DOB')
+        
+        self.tree.column('#4', anchor=CENTER, width=110)
+        self.tree.heading('#4', text='Age')
+        
+        self.tree.column('#5', anchor=CENTER, width=110)
+        self.tree.heading('#5', text='Gender')
+        
+        self.tree.column('#6', anchor=CENTER)
+        self.tree.heading('#6', text='City')
+        
+        self.tree.insert('', index=0, values=(1,"vijay", 18, "12-05-2007", 'male', 'chennai'))
+        self.tree.pack()
+        
+    def Add(self):
+        Id = self.Id_Entry.get()
+        name = self.Name_Entry.get()
+        age = self.Age_Entry.get()
+        dob = self.DOB_Entry.get()
+        gender = self.Gender_Entry.get()
+        city = self.Aqua_Entry.get()
+        db = sqlite3.connect("Student.db")
+        curses = db.cursor()
+        curses.execute("INSERT INTO Student(ID, NAME, AGE, DOB, GENDER, CITY) VALUES(?,?,?,?,?,?)", (Id,name,age,dob,gender,city))
+        db.commit()
+        db.close()
+        print("Value inserted")
+        self.tree.insert('', index=0, values=(Id,name,age,dob,gender,city))
+        
+    def Delete(self):
+        item = self.tree.selection()[0]
+        selected_items = self.tree.item(item)['values'][0]
+        db = sqlite3.connect("Student.db")
+        curses = db.cursor()
+        curses.execute("DELETE FROM Student WHERE ID={}".format(selected_items))
+        print("Value deleted")
+        db.commit()
+        db.close()
+        self.tree.delete(item)
+        
+    def Update(self):
+        Id = self.Id_Entry.get()
+        name = self.Name_Entry.get()
+        age = self.Age_Entry.get()
+        dob = self.DOB_Entry.get()
+        gender = self.Gender_Entry.get()
+        city = self.Aqua_Entry.get()
+        items = self.tree.selection()[0]
+        selcted_item = self.tree.item(items)['values'][0]
+        db = sqlite3.connect("Student.db")
+        cursor = db.cursor()
+        cursor.execute('UPDATE Student SET ID=?, NAME=?, AGE=?, DOB=?, GENDER=?, CITY=? WHERE ID=?',(selcted_item,name,age,dob,gender,city,selcted_item))
+        db.commit()
+        db.close()
+        print('Value updated')
+        self.tree.item(items, values=(Id, name,age,dob,gender,city))
+        
+    def Clear(self):
+        self.Id_Entry.delete(0, END)
+        self.Name_Entry.delete(0, END)
+        self.Age_Entry.delete(0, END)
+        self.DOB_Entry.delete(0, END)
+        self.Gender_Entry.delete(0, END)
+        self.Aqua_Entry.delete(0, END)
+
+main = Tk() #It creates the new window.
+main.title('Student Management Sysytem') #It changes the title of the window.
+main.resizable(False,False) #It does not allow us to resize the window.
+main.geometry('1200x600') #It fixes the size of the window.
+
+Student(main)
+main.mainloop()
--- Shooter-game/main.py
+++ Shooter-game/main.py
@@ -1,72 +1,129 @@
 import pygame
 import os
+pygame.font.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 900, 500
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Spacecraft Shooter')
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space-Shooter")
 
-BORDER = pygame.Rect((WIDTH/2) - 5, 0, 10, HEIGHT)
-BORDER_COLOR = (0,0,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
-BACKGROUND = (255,255,255)
+BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
 
-FPS = 60 #It defines the speed of the game in different computers.(Frames per second).
-VELOCITY = 3
-BULLET_VELOCITY = 7
-MAX_BULLETS = 5
+BULLET_HIT_SOUND = pygame.mixer.Sound('Shooter-game/Assets/Grenade+1.mp3')
+BULLET_FIRE_SOUND = pygame.mixer.Sound('Shooter-game/Assets/Gun+Silencer.mp3')
+
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+
+FPS = 60
+VEL = 5
+BULLET_VEL = 7
+MAX_BULLETS = 3
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Shooter-game','Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP =pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), 90)
-RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Shooter-game','Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), 270)
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Shooter-game','Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Shooter-game','Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+SPACE = pygame.transform.scale(pygame.image.load(
+    os.path.join('Shooter-game','Assets', 'bg.png')), (WIDTH, HEIGHT))
 
 
-def draw(red, yellow):
-    WINDOW.fill(BACKGROUND)
-    pygame.draw.rect(WINDOW, BORDER_COLOR, BORDER)
-    WINDOW.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
-    WINDOW.blit(RED_SPACESHIP, (red.x,red.y))
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    WIN.blit(SPACE, (0, 0))
+    pygame.draw.rect(WIN, BLACK, BORDER)
+
+    red_health_text = HEALTH_FONT.render(
+        "Life: " + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render(
+        "Life: " + str(yellow_health), 1, WHITE)
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WIN, RED, bullet)
+
+    for bullet in yellow_bullets:
+        pygame.draw.rect(WIN, YELLOW, bullet)
+
     pygame.display.update()
-    
-def yellow_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a] and yellow.x - VELOCITY > 0: #YELLOW LEFT
-        yellow.x -= VELOCITY
-    if keys_pressed[pygame.K_d] and yellow.x - VELOCITY + yellow.width < BORDER.x: #YELLOW RIGHT 
-        yellow.x += VELOCITY
-    if keys_pressed[pygame.K_w] and yellow.y - VELOCITY > 0: #YELLOW TOP
-        yellow.y -= VELOCITY
-    if keys_pressed[pygame.K_s] and yellow.y + VELOCITY + yellow.height < HEIGHT - 20: #YELLOW BOTTOM
-        yellow.y += VELOCITY
-        
-def red_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT] and red.x - VELOCITY > BORDER.x + BORDER.width: #RED LEFT
-        red.x -= VELOCITY
-    if keys_pressed[pygame.K_RIGHT] and red.x - VELOCITY + red.width < WIDTH: #RED RIGHT
-        red.x += VELOCITY
-    if keys_pressed[pygame.K_UP] and red.y - VELOCITY > 0: #RED TOP
-        red.y -= VELOCITY
-    if keys_pressed[pygame.K_DOWN] and red.y + VELOCITY + red.height < HEIGHT - 20: #RED BOTTOM
-        red.y += VELOCITY
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:  # DOWN
+        yellow.y += VEL
+
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # LEFT
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # RIGHT
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:  # DOWN
+        red.y += VEL
+
 
 def handle_bullets(yellow_bullets, red_bullets, yellow, red):
     for bullet in yellow_bullets:
-        bullet.x += BULLET_VELOCITY
-        if yellow.colliderect(bullet):
+        bullet.x += BULLET_VEL
+        if red.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
             yellow_bullets.remove(bullet)
 
+    for bullet in red_bullets:
+        bullet.x -= BULLET_VEL
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet)
+        elif bullet.x < 0:
+            red_bullets.remove(bullet)
+
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
+                         2, HEIGHT/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(4000)
 
 
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
-    
+
     red_bullets = []
     yellow_bullets = []
-    
+
+    red_health = 10
+    yellow_health = 10
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -74,27 +131,54 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height/2 - 2,10,5)
+                    bullet = pygame.Rect(
+                        yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
 
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(red.x , red.y + red.height/2 - 2,10,5)
+                    bullet = pygame.Rect(
+                        red.x, red.y + red.height//2 - 2, 10, 5)
                     red_bullets.append(bullet)
-        
+                    BULLET_FIRE_SOUND.play()
+
+            if event.type == RED_HIT:
+                red_health -= 1
+                BULLET_HIT_SOUND.play()
+
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
+                BULLET_HIT_SOUND.play()
+
+        winner_text = ""
+        if red_health <= 0:
+            winner_text = "Yellow Wins!"
+
+        if yellow_health <= 0:
+            winner_text = "Red Wins!"
+
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
+
         keys_pressed = pygame.key.get_pressed()
-        
-        yellow_movement(keys_pressed, yellow)
-        red_movement(keys_pressed, red)
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
 
         handle_bullets(yellow_bullets, red_bullets, yellow, red)
-            
-        draw(red, yellow)
-            
-    pygame.quit()
-    
-if __name__ == '__main__':#It prevents the file from running when it is imported.
+
+        draw_window(red, yellow, red_bullets, yellow_bullets,
+                    red_health, yellow_health)
+
     main()
-            
+
+
+if __name__ == "__main__":
+    main()  
+    
+
+          
